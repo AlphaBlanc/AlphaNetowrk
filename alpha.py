81db1cdad758d1864de4c308e5459aa6fc87b98d
AlphaBlanc <67218751+AlphaBlanc@users.noreply.github.com>
2020-06-21 16:18:50 +0900

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -11,10 +11,19 @@ async def on_ready():
     print("------------------")
     await bot.change_presence(game=discord.Game(name="서버주소: Alpha.minesv.kr", type=1))
 @bot.event
-@client.event
 async def on_message(message):
-    if message.content.startswith("!안녕"):
-        await message.channel.send("안녕하세요")
+    if "검색" == message.content.split(" ")[0]:
+        group = message.content.split(" ")[1]
+        search = requests.get("https://www.google.com/search?hl=ko&biw=958&bih=959&tbm=isch&sa=1&ei=L5ZtXJ2aLpGSr7wPiKuC-A0&q="+group)
+        bp = bs(search.text, "html.parser")
+        img = bp.find_all('img')
+        img2=img[2]
+        img_src=img2.get('src')
+        embed = discord.Embed(title="Erica:", description= "https://www.google.com/search?hl=ko&biw=958&bih=959&tbm=isch&sa=1&ei=L5ZtXJ2aLpGSr7wPiKuC-A0&q="+group, color=0x383b38)
+        embed.set_footer(icon_url="https://www.google.com/search?hl=ko&biw=958&bih=959&tbm=isch&sa=1&ei=L5ZtXJ2aLpGSr7wPiKuC-A0&q="+group)
+        embed.set_image(url=img_src)
+        await bot.send_message(message.channel, embed=embed)
+        del group, search, bp, img, embed
 
 access_token = os.environ["BOT_TOKEN"]
 bot.run(access_token)
