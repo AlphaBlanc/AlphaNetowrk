0badb964635f5e15be6ac4fa9da4f6ac5bd2ffff
AlphaBlanc <67218751+AlphaBlanc@users.noreply.github.com>
2020-06-21 16:14:02 +0900

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -11,5 +11,10 @@ async def on_ready():
     print("------------------")
     await bot.change_presence(game=discord.Game(name="서버주소: Alpha.minesv.kr", type=1))
 @bot.event
+@client.event
+async def on_message(message):
+    if message.content.startswith("!안녕"):
+        await message.channel.send("안녕하세요")
+
 access_token = os.environ["BOT_TOKEN"]
 bot.run(access_token)
